3e80f2d7a2e44cf274db2a69fe3fb60a22e7285c
daniel piva <imdanielpiva@gmail.com>
2017-09-15 17:12:42 -0300

    _added simple webpack config to bundle

---
+++ index.js
@@ -0,0 +1,15 @@
+//stuffs.js
+
+const $ = require('jquery');
+
+window.jQuery = $;
+window.$ = $;
+
+require('materialize-css/dist/js/materialize.js');
+require('materialize-css/dist/js/materialize.min.js');
+require('./js/scrollIt.js');
+
+//stuffs.css
+require('materialize-css/dist/css/materialize.css');
+require('materialize-css/dist/css/materialize.min.css');
+require('./css/style.css');
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const glob = require('glob');
+
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new ExtractTextPlugin("css/styles.css"),
+    new CopyWebpackPlugin([
+      { from: './images', to: 'images/'}
+    ]),
+    new UglifyJSPlugin()
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist/'),
+    publicPath: './', 
+    filename: 'js/bundle.js'
+  },
+  module: {
+      rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        loader: 'url-loader'
+      },
+      { 
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  }
+};
