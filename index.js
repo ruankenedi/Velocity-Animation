8a9a1fb084253c817561308f46db55288c7e86c4
daniel piva <imdanielpiva@gmail.com>
2017-09-22 10:23:37 -0300

    removed jquery require

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 //stuffs.js
-const $ = require('jquery');
-
-window.jQuery = $;
-window.$ = $;
-
-require('materialize-css/dist/js/materialize.js');
+require('materialize-css/dist/js/materialize.min.js');
 require('./src/js/scrollIt.js');
 require('./src/js/init.js');
 
 $('.header-background').append($('<video></video>')
-.addClass('video-background')
-.attr({
-  'src': require('./src/images/video.mp4'),
-  'autoplay': true,
-  'loop': true,
-  'muted': true
-})
+  .addClass('video-background')
+  .attr({
+    'src': require('./src/images/video.mp4'),
+    'autoplay': true,
+    'loop': true,
+    'muted': true
+  })
 );
 
 //stuffs.css
